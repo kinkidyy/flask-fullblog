12a9ee07ac319ae1936e181a1331f58c00d94b15
kinkidy <ahmadidi007@gmail.com>
2025-11-07 23:47:32 +0100

    homepage redirect added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import redirect, url_for
 from app import create_app
+from flask import redirect, url_for
 from app.models import db
 
 # Create the app
@@ -12,4 +12,4 @@ def home():
     return redirect(url_for('blog.index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
